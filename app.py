c9e2ff6f921e8178a8a99d151fd008aead62cf79
sameer-rahman <sameer_r@berkeley.edu>
2024-10-20 02:51:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/halal_meals_today', methods=['GET'])
 def get_halal_meals():
-    halal_meals_data = mealClassifier.get_halal_meals_for_today()
+    halal_meals_data = mealClassifier.swap_dict_layers(mealClassifier.get_halal_meals_for_today())
     return jsonify(halal_meals_data)
 
 @app.route('/vegan_meals_today', methods = ['GET'])
 def get_vegan_meals():
-    vegan_meals_data = mealClassifier.get_vegan_meals_for_today()
+    vegan_meals_data = mealClassifier.swap_dict_layers(mealClassifier.get_vegan_meals_for_today())
     return jsonify(vegan_meals_data)
 
 @app.route('/vegetarian_meals_today', methods = ['GET'])
 def get_vegetarian_meals():
-    vegetarian_meals_data = mealClassifier.get_vegetarian_meals_for_today()
+    vegetarian_meals_data = mealClassifier.swap_dict_layers(mealClassifier.get_vegetarian_meals_for_today())
     return jsonify(vegetarian_meals_data)
